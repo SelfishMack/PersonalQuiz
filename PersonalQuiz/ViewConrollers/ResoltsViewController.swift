27170932299bb8bf9462886b5a42106d4615d6e3
SelfishMak <selfish.mak@gmail.com>
2021-11-15 20:56:48 +0300

    final

--- PersonalQuiz/ViewConrollers/ResoltsViewController.swift
+++ PersonalQuiz/ViewConrollers/ResoltsViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class ResoltsViewController: UIViewController {
+import UIKit
 
+class ResultsViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+    
+    @IBOutlet weak var animalTypeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+ //       navigationItem.hidesBackButton = true
+        updateResult()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// MARK: - Private Methods
+extension ResultsViewController {
+    
+    private func updateResult() {
+        
+        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        let animals = answers.map { $0.type }
+        
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+        for animal in animals {
+            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        updateUI(with: mostFrequencyAnimal)
+    }
+    
+    private func updateUI(with animal: AnimalType?) {
+        animalTypeLabel.text = "Вы - \(animal?.rawValue ?? "🐶" )!"
+        descriptionLabel.text = animal?.definition ?? ""
+    }
+}
+
